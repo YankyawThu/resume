7cdd4649bdc314ebbad87e4a384813e8a17e0068
Yan <yan@Yans-MacBook-Pro.local>
2024-04-29 04:18:14 +0700

    rm animation

--- app/page.js
+++ app/page.js
@@ -10,8 +10,6 @@ import { IoLogoJavascript, IoPeopleOutline, IoLocationOutline } from "react-icon
 import { BsFiletypeScss } from "react-icons/bs";
 import { motion, useScroll } from "framer-motion"
 import Link from 'next/link'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import { Nunito } from "next/font/google";
 
 const nunito = Nunito({
@@ -31,7 +29,6 @@ export default function Home() {
     // setTimeout(() => {
     //   setWelcomeMsg(false)
     // }, 5000)
-    AOS.init()
     mode(localStorage.theme)
   }, [])
 
@@ -75,7 +72,7 @@ export default function Home() {
       <div className="flex flex-row justify-center py-3 items-center">
         <div className="relative mr-5">
           <Image src="/me.png" width={50} height={30} alt="Yan" className="rounded-full" />
-          <Image src="/welcomeText.png" width={100} height={100} data-aos="fade-down-left" data-aos-duration="2000" alt="welcome" className="absolute max-w-none w-[150px] h-[90px] top-6 right-10" />
+          <Image src="/welcomeText.png" width={100} height={100} alt="welcome" className="absolute max-w-none w-[150px] h-[90px] top-6 right-10" />
         </div>
         {theme == 'dark' ? 
           <button className="p-1 bg-gray-700 rounded-full" onClick={() => mode('light')}>
@@ -130,15 +127,13 @@ export default function Home() {
 
   const Summary = () => {
     return (
-      <div data-aos="fade-up" data-aos-duration="500">
-        <div className="skew flex flex-col lg:flex-row justify-center items-center my-5 lg:my-[60px] mx-8 lg:mx-[100px]">
-          <div className="inner grow">
-            <div className="text-2xl pt-3 text-[#EC4899]">What I am..</div>
-              <p className="text-gray-500 dark:text-gray-400 pt-2">I'm a passionate and results-driven {position} with <span className="text-black dark:text-white">{exp}</span> exp and a deep love for technology.A web developer with expertise in responsive web design and API development. Strong at developing and maintaining high-quality web applications and quickly understanding project architecture. Excellent problem solver known for delivering projects on time and staying up-to-date with web development trends.</p>
-          </div>
-          <div className="flex-none w-96 mt-8 lg:mt-0">
-            <Image src="/coding.gif" width={80} height={80} alt="coding" unoptimized className="m-auto w-56 h-56 rounded-full" />
-          </div>
+      <div className="skew flex flex-col lg:flex-row justify-center items-center my-5 lg:my-[60px] mx-8 lg:mx-[100px]">
+        <div className="inner grow">
+          <div className="text-2xl pt-3 text-[#EC4899]">What I am..</div>
+            <p className="text-gray-500 dark:text-gray-400 pt-2">I'm a passionate and results-driven {position} with <span className="text-black dark:text-white">{exp}</span> exp and a deep love for technology.A web developer with expertise in responsive web design and API development. Strong at developing and maintaining high-quality web applications and quickly understanding project architecture. Excellent problem solver known for delivering projects on time and staying up-to-date with web development trends.</p>
+        </div>
+        <div className="flex-none w-96 mt-8 lg:mt-0">
+          <Image src="/coding.gif" width={80} height={80} alt="coding" unoptimized className="m-auto w-56 h-56 rounded-full" />
         </div>
       </div>
     )
@@ -146,41 +141,39 @@ export default function Home() {
 
   const Skills = () => {
     return (
-      <div data-aos="fade-right" data-aos-duration="500">
-        <div className="skew1 my-10 mx-5 lg:mx-0 lg:mt-0">
-          <div className="inner">
-            <div className="text-2xl text-[#EC4899]">What I can..</div>
-            <div className="flex flex-col text-gray-500 pt-2">
-              <div className="inline-flex items-center my-1">
-                <span className="text-gray-500 dark:text-gray-400">Backend</span> -
-                <SiPhp className="text-3xl mx-2 text-indigo-500" />
-                <FaLaravel className="text-2xl mx-2 text-red-500" />
+      <div className="skew1 my-10 mx-5 lg:mx-0 lg:mt-0">
+        <div className="inner">
+          <div className="text-2xl text-[#EC4899]">What I can..</div>
+          <div className="flex flex-col text-gray-500 pt-2">
+            <div className="inline-flex items-center my-1">
+              <span className="text-gray-500 dark:text-gray-400">Backend</span> -
+              <SiPhp className="text-3xl mx-2 text-indigo-500" />
+              <FaLaravel className="text-2xl mx-2 text-red-500" />
+            </div>
+            <div className="inline-flex items-start my-1">
+              <span className="text-gray-500 dark:text-gray-400">Frontend</span> -
+              <div className="flex flex-wrap flex-row">
+                <FaHtml5 className="text-2xl mx-2 my-2 text-orange-500" />
+                <FaCss3 className="text-2xl mx-2 my-2 text-blue-500" />
+                <IoLogoJavascript className="text-2xl mx-2 my-2 text-yellow-500" />
+                <BsFiletypeScss className="text-2xl mx-2 my-2 text-black dark:text-white" />
+                <FaBootstrap className="text-2xl mx-2 my-2 text-indigo-700" />
+                <SiTailwindcss className="text-2xl mx-2 my-2 text-cyan-500" />
+                <SiJquery className="text-2xl mx-2 my-2 text-blue-300" />
+                <SiInertia className="text-2xl mx-2 my-2 text-black dark:text-white" />
+                <SiVuedotjs className="text-2xl mx-2 my-2 text-green-600" />
+                <FaReact className="text-2xl mx-2 my-2 text-cyan-500" />
+                <SiNextdotjs className="text-2xl mx-2 my-2 text-black dark:text-white" />
               </div>
-              <div className="inline-flex items-start my-1">
-                <span className="text-gray-500 dark:text-gray-400">Frontend</span> -
-                <div className="flex flex-wrap flex-row">
-                  <FaHtml5 className="text-2xl mx-2 my-2 text-orange-500" />
-                  <FaCss3 className="text-2xl mx-2 my-2 text-blue-500" />
-                  <IoLogoJavascript className="text-2xl mx-2 my-2 text-yellow-500" />
-                  <BsFiletypeScss className="text-2xl mx-2 my-2 text-black dark:text-white" />
-                  <FaBootstrap className="text-2xl mx-2 my-2 text-indigo-700" />
-                  <SiTailwindcss className="text-2xl mx-2 my-2 text-cyan-500" />
-                  <SiJquery className="text-2xl mx-2 my-2 text-blue-300" />
-                  <SiInertia className="text-2xl mx-2 my-2 text-black dark:text-white" />
-                  <SiVuedotjs className="text-2xl mx-2 my-2 text-green-600" />
-                  <FaReact className="text-2xl mx-2 my-2 text-cyan-500" />
-                  <SiNextdotjs className="text-2xl mx-2 my-2 text-black dark:text-white" />
-                </div>
-              </div>
-              <div className="inline-flex items-center my-1">
-                <span className="text-gray-500 dark:text-gray-400">Database</span> -
-                <SiMysql className="text-[40px] mx-2 text-blue-800" />
-              </div>
-              <div className="inline-flex items-center my-1">
-                <span className="text-gray-500 dark:text-gray-400">CI/CD</span> -
-                <FaGithub className="text-2xl mx-2 text-black dark:text-white" />
-                <SiGitlab className="text-2xl mx-2 text-orange-600" />
-              </div>
+            </div>
+            <div className="inline-flex items-center my-1">
+              <span className="text-gray-500 dark:text-gray-400">Database</span> -
+              <SiMysql className="text-[40px] mx-2 text-blue-800" />
+            </div>
+            <div className="inline-flex items-center my-1">
+              <span className="text-gray-500 dark:text-gray-400">CI/CD</span> -
+              <FaGithub className="text-2xl mx-2 text-black dark:text-white" />
+              <SiGitlab className="text-2xl mx-2 text-orange-600" />
             </div>
           </div>
         </div>
@@ -190,93 +183,89 @@ export default function Home() {
 
   const RoadMap = () => {
     return (
-      <div data-aos="fade-left" data-aos-duration="500">
-        <div className="timeline flex-none w-auto lg:w-[550px] self-center my-14 lg:my-0">
-          <div className="text-2xl text-[#EC4899] text-center mb-10">How I'v been growing..</div>
-          <ul>
-            <li className="left">
-              <div className="content mt-10" data-aos="zoom-in" data-aos-duration="1000">
-                <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><Image src="/tamron.png" width={28} height={28} alt="tamron" />Tamron Technology</div>
-                <div className="text-xs">Web Developer</div>
-              </div>
-              <div className="time">
-                <div>Now 2024</div>
-              </div>
-            </li>
+      <div className="timeline flex-none w-auto lg:w-[550px] self-center my-14 lg:my-0">
+        <div className="text-2xl text-[#EC4899] text-center mb-10">How I'v been growing..</div>
+        <ul>
+          <li className="left">
+            <div className="content mt-10">
+              <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><Image src="/tamron.png" width={28} height={28} alt="tamron" />Tamron Technology</div>
+              <div className="text-xs">Web Developer</div>
+            </div>
+            <div className="time">
+              <div>Now 2024</div>
+            </div>
+          </li>
 
-            <li className="right">
-              <div className="content mt-10" data-aos="zoom-in" data-aos-duration="1500">
-                <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><BsBuildings className="mr-1" />Freelance</div>
-                <div className="text-xs">Backend Developer</div>
-              </div>
-              <div className="time">
-                <div>Apr 2024</div>
-              </div>
-            </li>
+          <li className="right">
+            <div className="content mt-10">
+              <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><BsBuildings className="mr-1" />Freelance</div>
+              <div className="text-xs">Backend Developer</div>
+            </div>
+            <div className="time">
+              <div>Apr 2024</div>
+            </div>
+          </li>
 
-            <li className="right">
-              <div className="content text-gray-500 mt-10">
-              </div>
-              <div className="time">
-                <div>Jan 2024</div>
-              </div>
-            </li>
+          <li className="right">
+            <div className="content text-gray-500 mt-10">
+            </div>
+            <div className="time">
+              <div>Jan 2024</div>
+            </div>
+          </li>
 
-            <li className="left">
-              <div className="content mt-10" data-aos="zoom-in" data-aos-duration="2000">
-                <div className="flex flex-inline text-gray-500 dark:text-gray-400 items-center"><BsBuildings className="mr-1" />Freelance</div>
-                <div className="text-xs">Web Developer</div>
-              </div>
-              <div className="time">
-                <div>Nov 2023</div>
-              </div>
-            </li>
+          <li className="left">
+            <div className="content mt-10">
+              <div className="flex flex-inline text-gray-500 dark:text-gray-400 items-center"><BsBuildings className="mr-1" />Freelance</div>
+              <div className="text-xs">Web Developer</div>
+            </div>
+            <div className="time">
+              <div>Nov 2023</div>
+            </div>
+          </li>
 
-            <li className="left">
-              <div className="content text-gray-500 mt-10">
-              </div>
-              <div className="time">
-                <div>Aug 2023</div>
-              </div>
-            </li>
-            
-            <li className="left">
-              <div className="time">
-                <div>Sep 2020</div>
-              </div>
-            </li>
+          <li className="left">
+            <div className="content text-gray-500 mt-10">
+            </div>
+            <div className="time">
+              <div>Aug 2023</div>
+            </div>
+          </li>
+          
+          <li className="left">
+            <div className="time">
+              <div>Sep 2020</div>
+            </div>
+          </li>
 
-            <li className="right">
-              <div className="content mt-10" data-aos="zoom-in" data-aos-duration="2500">
-                <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><Image src="/sh.png" width={25} height={25} alt="sh" className="mr-1" />SweetyHome Industry</div>
-                <div className="text-xs">IT Programmer</div>
-              </div>
-              <div className="time">
-                <div>Aug 2020</div>
-              </div>
-            </li>
-            
-            <li className="right">
-              <div className="time">
-                <div>Oct 2019</div>
-              </div>
-            </li>
-            <div style={{clear:'both'}}></div>
-          </ul>
-        </div>
+          <li className="right">
+            <div className="content mt-10">
+              <div className="inline-flex text-gray-500 dark:text-gray-400 items-center"><Image src="/sh.png" width={25} height={25} alt="sh" className="mr-1" />SweetyHome Industry</div>
+              <div className="text-xs">IT Programmer</div>
+            </div>
+            <div className="time">
+              <div>Aug 2020</div>
+            </div>
+          </li>
+          
+          <li className="right">
+            <div className="time">
+              <div>Oct 2019</div>
+            </div>
+          </li>
+          <div style={{clear:'both'}}></div>
+        </ul>
       </div>
     )
   }
 
   const Language = () => {
     return (
-      <div data-aos="fade-right" data-aos-duration="500">
-        <div className="skew2 lg:w-full mt-5 mx-3 lg:mx-0 lg:mt-10">
-          <div className="inner">
-            <div className="text-2xl pt-3 text-[#EC4899]">How I speak..</div>
-            <div className="flex flex-col justify-center items-center mt-2">
-              <Image src="/communication.png" width={200} height={200} alt="talk" />
-            </div>
+      <div className="skew2 lg:w-full mt-5 mx-3 lg:mx-0 lg:mt-10">
+        <div className="inner">
+          <div className="text-2xl pt-3 text-[#EC4899]">How I speak..</div>
+          <div className="flex flex-col justify-center items-center mt-2">
+            <Image src="/communication.png" width={200} height={200} alt="talk" />
           </div>
         </div>
       </div>
@@ -285,24 +274,22 @@ export default function Home() {
 
   const School = () => {
     return (
-      <div data-aos="fade-up" data-aos-duration="500">
-        <div className="skew3 flex flex-col m-10 lg:my-0 lg:mx-[100px]">
-          <div className="text-2xl text-[#EC4899]">What I have..</div>
-          <div className="flex flex-row">
-            <div className="inner flex-none mt-2 w-auto lg:w-60">
-              <div className="hidden lg:block relative w-56 h-56">
-                <Image src="/uni.jpeg" width={100} height={100} className="card card04" alt="photo" />
-                <Image src="/uni2.png" width={100} height={100} className="card card06" alt="photo" />
-                <Image src="/uni4.jpeg" width={100} height={100} className="card card07" alt="photo" />
+      <div className="skew3 flex flex-col m-10 lg:my-0 lg:mx-[100px]">
+        <div className="text-2xl text-[#EC4899]">What I have..</div>
+        <div className="flex flex-row">
+          <div className="inner flex-none mt-2 w-auto lg:w-60">
+            <div className="hidden lg:block relative w-56 h-56">
+              <Image src="/uni.jpeg" width={100} height={100} className="card card04" alt="photo" />
+              <Image src="/uni2.png" width={100} height={100} className="card card06" alt="photo" />
+              <Image src="/uni4.jpeg" width={100} height={100} className="card card07" alt="photo" />
+            </div>
+          </div>
+          <div className="sch grow flex flex-col items-center">
+              <div className="sch-name inline-flex items-center pt-5 lg:pt-14">
+                <Image src="/uni3.png" width={70} height={70} alt="uni" />
+                University of Computer Studies, Yangon, Myanmar
               </div>
-            </div>
-            <div className="sch grow flex flex-col items-center">
-                <div className="sch-name inline-flex items-center pt-5 lg:pt-14">
-                  <Image src="/uni3.png" width={70} height={70} alt="uni" />
-                  University of Computer Studies, Yangon, Myanmar
-                </div>
-                <div className="py-2 lg:py-10 text-lg text-gray-500 dark:text-gray-400">Bachelor Degree of Computer Science (2020)</div>
-            </div>
+              <div className="py-2 lg:py-10 text-lg text-gray-500 dark:text-gray-400">Bachelor Degree of Computer Science (2020)</div>
           </div>
         </div>
       </div>
@@ -314,7 +301,7 @@ export default function Home() {
       <div className="projects my-14 lg:my-[100px]">
         <div className="text-2xl text-[#EC4899] text-center mb-5">What I did..</div>
         <div className="grid grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-y-8 lg:gap-x-8 mx-5 justify-items-center">
-          <div className="proj-card shadow-lg shadow-cyan-500/50" data-aos="fade-right" data-aos-duration="500">
+          <div className="proj-card shadow-lg shadow-cyan-500/50">
             <span className="icon">
               <IoPeopleOutline className="text-[12px] text-gray-500 dark:text-gray-400" />
             </span>
@@ -344,7 +331,7 @@ export default function Home() {
               <div className="line line-3 before:bg-[#e2e2e2] after:bg-[#e2e2e2] dark:before:bg-[#2a2b2c] dark:after:bg-[#2a2b2c]"></div>
             </div>
           </div>
-          <div className="proj-card shadow-lg shadow-cyan-500/50" data-aos="fade-up" data-aos-duration="500">
+          <div className="proj-card shadow-lg shadow-cyan-500/50">
             <span className="icon">
               <MdOutlineAttachMoney className="text-[12px] text-gray-500 dark:text-gray-400" />
             </span>
@@ -373,7 +360,7 @@ export default function Home() {
               <div className="line line-3 before:bg-[#e2e2e2] after:bg-[#e2e2e2] dark:before:bg-[#2a2b2c] dark:after:bg-[#2a2b2c]"></div>
             </div>
           </div>
-          <div className="proj-card shadow-lg shadow-cyan-500/50" data-aos="fade-left" data-aos-duration="500">
+          <div className="proj-card shadow-lg shadow-cyan-500/50">
             <span className="icon">
               <FaOpencart className="text-[12px] text-gray-500 dark:text-gray-400" />
             </span>
