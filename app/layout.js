247e7a4ff534c7d6ff1b633483d32031b80e92da
Yan <yan@Yans-MacBook-Pro.local>
2024-04-29 13:33:47 +0700

    fx favicon

--- app/layout.js
+++ app/layout.js
@@ -6,12 +6,15 @@ const comfortaa = Comfortaa({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Yan",
-  description: "Take a look",
+  description: "Take a look"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" type="image/png" sizes="32x32"></link>
+      </head>
       <body className={comfortaa.className + ' bg-white dark:bg-black text-black dark:text-white'}>{children}</body>
     </html>
   );
